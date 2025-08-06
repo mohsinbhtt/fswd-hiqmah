4ebd0647c396f9f5c74cd8b868249f58f963bf84
mohsinbhtt <mohsinbh141@gmail.com>
2025-08-06 05:36:38 -0700

    modified

--- Java script/index.js
+++ Java script/index.js
@@ -1,1 +1,62 @@
-console.log('hello world');
+
+// let age = 28;
+// let weight = 68;  
+// let height = 1.72; 
+
+
+// let bmi= weight / (height * height);
+// console.log(bmi)
+
+// if (bmi < 18) {
+//   console.log('under weight');
+// } else {
+//   if (bmi < 40) {
+//     console.log('need to improve')
+//   } else {
+//     if (bmi <70) {
+//       console.log('over weight');
+//     } else {
+//       console.log('too weak')
+//     }
+//   }
+// }
+// let input1 = prompt('enter a number')
+// if(Number (input1) %2 === 0){
+//   console.log('its a even numbr')
+// }else{
+//     console.log('odd number')
+// }
+
+
+let age = 65;
+let isStudent = false;
+let isWeekend = true;
+
+let ticketPrice;
+
+if (age < 5) {
+  ticketPrice = 0;
+} else { 
+  
+  let standardPrice;
+  if (isWeekend) {
+    standardPrice = 300;
+  } else {
+    standardPrice = 200;
+  }
+
+  
+  if (age >= 60) {
+
+    ticketPrice = standardPrice * 0.7;
+  } else if (isStudent && !isWeekend) {
+   
+    ticketPrice = standardPrice * 0.5;
+  } else {
+   
+    ticketPrice = standardPrice;
+  }
+}
+
+
+console.log("Final Ticket Price:", "₹" + ticketPrice);
